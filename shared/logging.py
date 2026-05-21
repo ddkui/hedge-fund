ecdd5914854037d28c7417a8e852d362fd6ee74c
jomik <dannjeru555@gmail.com>
2026-05-21 19:34:19 +0200

    feat: structured JSON logger

---
+++ shared/logging.py
@@ -0,0 +1,15 @@
+import structlog
+
+
+def get_logger(agent_name: str) -> structlog.BoundLogger:
+    structlog.configure(
+        processors=[
+            structlog.stdlib.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.JSONRenderer(),
+        ],
+        wrapper_class=structlog.BoundLogger,
+        context_class=dict,
+        logger_factory=structlog.PrintLoggerFactory(),
+    )
+    return structlog.get_logger().bind(agent=agent_name)
